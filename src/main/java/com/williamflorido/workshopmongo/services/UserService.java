0d898b828daefcb60ce7cf6058dcd8034944fd9b
William Florido <william.frdo@gmail.com>
2020-05-20 16:53:26 -0300

    Inserção de usuário com POST

--- src/main/java/com/williamflorido/workshopmongo/services/UserService.java
+++ src/main/java/com/williamflorido/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.williamflorido.workshopmongo.domain.User;
+import com.williamflorido.workshopmongo.dto.UserDTO;
 import com.williamflorido.workshopmongo.repository.UserRepository;
 import com.williamflorido.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -23,5 +24,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
